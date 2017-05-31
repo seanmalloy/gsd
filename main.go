7b193f44edf30da5f3113ea0b673ca4d8d762d5e
Sean Malloy <spinelli85@gmail.com>
2017-05-30 23:16:17 -0500

    Write function to do DNS lookups

--- main.go
+++ main.go
@@ -5,7 +5,14 @@ import (
 	"net"
 )
 
+func get_dns_names(names []string) {
+	for _, n := range names {
+		addr, _ := net.LookupHost(n)
+		fmt.Println(n, addr[0]) // TODO what to do with multiple results?
+	}
+}
+
 func main() {
-	var name string = "www.google.com"
-	fmt.Println(net.LookupHost(name))
+	names := []string{"www.google.com", "www.openbsd.org"}
+	get_dns_names(names)
 }
