815a013ebc5a2eb0b93e87b9333e75939faba071
Sean Malloy <spinelli85@gmail.com>
2017-06-03 23:06:24 -0500

    Add simple function to ping

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/tatsushid/go-fastping"
 	"net"
+	"os"
+	"time"
 )
 
 func getDnsNames(names []string) {
@@ -12,7 +15,31 @@ func getDnsNames(names []string) {
 	}
 }
 
+// TODO requires root
+func pingHosts(name string) {
+	p := fastping.NewPinger()
+	//p.Network("udp")
+	ra, err := net.ResolveIPAddr("ip4:icmp", name)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	p.AddIPAddr(ra)
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+	}
+	p.OnIdle = func() {
+		fmt.Println("finish")
+	}
+	err = p.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
 	names := []string{"www.google.com", "www.openbsd.org"}
 	getDnsNames(names)
+
+	pingHosts("8.8.8.8")
 }
