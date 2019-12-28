a089fde7b437dba629512b6a6b585be8cf55f489
Sean Malloy <spinelli85@gmail.com>
2019-12-28 00:15:04 -0600

    pkg/version: initial version package
    * Variables for storing app build info
    * Function to print app version

---
+++ pkg/version/version.go
@@ -0,0 +1,22 @@
+package version
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// Application build information.
+var (
+	Branch    string
+	BuildDate string
+	GitSHA1   string
+	Version   = "v0.0.1-dev"
+)
+
+// Print writes application version details to standard output.
+func Print() {
+	// TODO remove hard coded "gsd" string here
+	fmt.Printf("gsd, version %v (branch: %v, revision: %v)\n", Version, Branch, GitSHA1)
+	fmt.Println("build date:", BuildDate)
+	fmt.Println("go version:", runtime.Version())
+}
