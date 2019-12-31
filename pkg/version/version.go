1bcce2c25c57b41dcf2ca458a018ca255461dcc3
Sean Malloy <spinelli85@gmail.com>
2019-12-31 00:19:52 -0600

    chore: bump version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -10,7 +10,7 @@ var (
 	Branch    string
 	BuildDate string
 	GitSHA1   string
-	Version   = "v0.0.2-dev"
+	Version   = "v0.0.2"
 )
 
 // Print writes application version details to standard output.
